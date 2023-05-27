cb19188dd87f2c2a06c88866b77c9a2c05527781
Michael Walters <Michael1996Walters@gmail.com>
2023-05-27 16:06:42 +0100

    fixed issue created during merge

--- assets/js/weatherAPI.js
+++ assets/js/weatherAPI.js
@@ -1,33 +1,81 @@
-let cityinput = document.getElementById("cityinput");
-let inputbutton = document.getElementById("search");
-var cardholder = document.getElementById("daycontainer");
+let cityinput = document.getElementById("cityinput"); //Form the user entered data into
+let inputbutton = document.getElementById("search"); //Sumbit button on form
+var cardholder = document.getElementById("daycontainer"); //the container for each day in 5 day forecast
+var searchitems = document.getElementById("history"); //container for search history
+var today = document.getElementById("today"); //the container for todays info
+var searches = JSON.parse(localStorage.getItem("searchItems")) || []; //get the search items from local storage
 let searchCity; //variable for parsed user input - in format for search in lowercase
 let historyCity; //variable for user input - in format for search history - in Titlecase
 
 let APIkey = "2ae4997dd0cb7de82a061647d700fe95";
 
-var now = dayjs();
+function searchHistory(newterm) {
+  //function to update and display search history
+  if (!searches.includes(newterm) && newterm) {
+    //if old searches doesnt include the new search term also make sure there is a new term
+    searches.push(newterm); //push the new term onto the search array
+  }
+  if (searches.length > 5) {
+    //if the history reaches is over 5, get rid of the first one (oldest)
+    searches.shift();
+  }
+  searchitems.innerHTML = ""; //wipe the search history
+  for (let index = searches.length - 1; index >= 0; index--) {
+    //runs a for loop, but starts at the last object and runs backwards so the most recent search is at the top
+    var prevSearch = document.createElement("li"); //make a list item
+    prevSearch.textContent = searches[index]; //writes the search history item into the list item
+    searchitems.appendChild(prevSearch); //attach to the search history
+  }
 
-let days = function (offset) {
-  //function to offset days for 5 day forcast days(0) returns today, day(1) returns tomorrow, completely unneeded
+  localStorage.setItem("searchItems", JSON.stringify(searches)); //store the search history
+}
 
-  return now.add(offset, "day").format("DD-MM-YYYY");
-};
 function mapWeatherData(obj) {
-  parsedWeather = {};
-  parsedWeather.date = obj.date;
+  //take in the response object
+  parsedWeather = {}; //make a new object
+  parsedWeather.date = obj.date; //only parse in the data we want
   parsedWeather.icon = `https://openweathermap.org/img/wn/${obj.weather[0].icon}@2x.png`; //the url needed for the icon
-  parsedWeather.temp = obj.main.temp;
+  parsedWeather.temp = obj.main.temp; //simplifies longer paths to keys like these
   parsedWeather.wind = obj.wind.speed;
   parsedWeather.humid = obj.main.humidity;
-  return parsedWeather;
+  return parsedWeather; //returns the new object
+}
+
+function writeToday(City, obj) {
+  //display the data for the today section
+  today.innerHTML = ""; //clear the section to remove the last search information
+
+  var cityAndDate = document.createElement("p"); //create a new text element (repeat for all items)
+  cityAndDate.textContent = `${City} ${obj.date}`; //inputs data from the supplied object (repeat for all items)
+  var icon = document.createElement("img");
+  icon.src = obj.icon; //source for the image is already parsed to a url by the mapWeatherData function
+  var temp = document.createElement("p");
+  temp.textContent = `Temp: ${obj.temp} C`;
+  var wind = document.createElement("p");
+  wind.textContent = `Wind: ${obj.wind} MPH`;
+  var humid = document.createElement("p");
+  humid.textContent = `Humidity ${obj.humid}%`;
+  if (obj.humid < 30) {
+    //apply a class if humidity is within certain bounds
+    humid.className = "dry";
+  } else if (obj.humid < 60) {
+    humid.className = "moderate";
+  } else {
+    humid.className = "humid";
+  }
+  today.appendChild(cityAndDate); //append all the information to the today element
+  today.appendChild(icon);
+  today.appendChild(temp);
+  today.appendChild(wind);
+  today.appendChild(humid);
 }
 
 function writeCard(obj) {
-  var card = document.createElement("card");
+  //function to write one card for 1 days forecast
+  var card = document.createElement("card"); //make a card
 
-  var date = document.createElement("p");
-  date.textContent = obj.date;
+  var date = document.createElement("p"); //make text element for each parameter of the forecast
+  date.textContent = obj.date; //pass in the data
   var icon = document.createElement("img");
   icon.src = obj.icon;
   var temp = document.createElement("p");
@@ -36,12 +84,12 @@ function writeCard(obj) {
   wind.textContent = `Wind: ${obj.wind} MPH`;
   var humid = document.createElement("p");
   humid.textContent = `Humidity ${obj.humid}%`;
-  card.appendChild(date);
+  card.appendChild(date); //append all the elements to the card (one day forecast)
   card.appendChild(icon);
   card.appendChild(temp);
   card.appendChild(wind);
   card.appendChild(humid);
-  cardholder.append(card);
+  cardholder.append(card); //append the card into the card holder (which will hold the full 5 cards for a 5 day forecast)
 }
 
 function getWeather(city) {
@@ -51,9 +99,11 @@ function getWeather(city) {
     city +
     "&appid=" +
     APIkey +
-    "&units=metric";
+    "&units=metric"; //creates the request using data provided by user
   fetch(request).then(function (response) {
+    //makes the request and awaits a response
     if (response.ok) {
+      //if the response was valid
       response.json().then(function (data) {
         let weather = data.list; //this has weather for every 3 hrs
         weather = weather.filter((_, index) => index % 8 === 0); //makes it one per 24 hrs
@@ -62,25 +112,41 @@ function getWeather(city) {
           //adds nicely formatted date key value pair
           return { ...object, date };
         });
-        cardholder.innerHTML = ""; //empties the container completely
+        writeToday(city, mapWeatherData(weather[0])); //calls the writeToday function to display todays weather - first item in the weather array
+        cardholder.innerHTML = ""; //empties the 5 day forecast container completely
         for (let index = 0; index < 5; index++) {
           const element = weather[index]; //gets just one of the objects (one days weather info)
           writeCard(mapWeatherData(element)); //makes a new object parsedWeather with easier paths to keys and only the keys we need
-          //now have object with just the needed info
-          //write code to create elements here
         }
       });
+
+      historyCity = searchCity.charAt(0).toUpperCase() + searchCity.substr(1); //puts input to Titlecase for search history (personal choice)
+      searchHistory(historyCity); //calls the searchHistory function to put data into the search history
+    } else {
+      alert("City not found"); //tell the user if we didnt get a positive response
     }
   });
 }
 
 function search() {
   searchCity = cityinput.value.trim().toLowerCase(); //puts input to lowercase for fetch
-  historyCity = searchCity.charAt(0).toUpperCase() + searchCity.substr(1); //puts input to Titlecase for search history (personal choice)
-  getWeather(searchCity);
+  getWeather(searchCity); //starts function to get data
 }
 
 inputbutton.addEventListener("submit", (event) => {
-  event.preventDefault();
-  search();
+  //if the search button is clicked
+  event.preventDefault(); //dont reset
+  search(); //start the search
 });
+
+searchitems.addEventListener("click", (event) => {
+  //if a search item is clicked
+  if (event.target.tagName == "LI") {
+    //check that it was a list item
+    var clickedsearch = event.target.innerHTML; //get the contents of the clicked list item
+    clickedsearch = clickedsearch.toLowerCase(); //put it to lowercase
+    getWeather(clickedsearch); //start a search for it
+  }
+});
+
+searchHistory(); //loads the search history on startup
